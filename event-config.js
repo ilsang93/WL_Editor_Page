a9210768cd4d8185ebbe4edd21c4cb04b7f873f2
ilsang <pchm20@gmail.com>
2025-10-07 03:46:48 +0900

    2025-10-07 스피스시프트 이벤트 추가

--- event-config.js
+++ event-config.js
@@ -53,6 +53,10 @@ export const EVENT_IDS_BY_TYPE = {
         'illusion',
         'flame'
     ],
+    'speedshift': [
+        'speedup',
+        'speeddown'
+    ],
     'custom': [] // custom 타입은 빈 배열 (텍스트 입력 사용)
 };
 
